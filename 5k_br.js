13eaedaac7450b31950039aa5716c39836a8cca1
spriteololo <30690634+spriteololo@users.noreply.github.com>
2020-11-08 22:04:42 +0300

    Add files via upload

--- 5k_br.js
+++ 5k_br.js
@@ -106,7 +106,7 @@ function looper() {
     log.v("timerCountDown = " + timerCountDown )
     if(timerCountDown == "-1") { // Timer is off
         log.i("response = " + overlayResponse + ", currentState = " + currentState);
-        if(overlayResponse == 'Вы должны иметь в руках "Кирка рудокопа" или "Золотая кирка рудокопа"') {
+        if(overlayResponse == 'Вы должны иметь в руках "Золотая кирка рудокопа" или "Кирка рудокопа"') {
             stopLoop();
             var promise = new Promise(function (resolve) {
                 jQuery.get('https://5kings.ru/bag_type_17.chtml', function (response) {
